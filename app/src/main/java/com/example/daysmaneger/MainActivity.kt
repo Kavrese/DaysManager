c1cc5568a17b4015c21b342aed0722b4670d7a7e
Артемий <va2005art@gmail.com>
2020-12-10 20:39:24 +0300

    add Start View

--- app/src/main/java/com/example/daysmaneger/MainActivity.kt
+++ app/src/main/java/com/example/daysmaneger/MainActivity.kt
@@ -2,10 +2,30 @@ package com.example.daysmaneger
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.view.View
+import android.widget.Toast
+import androidx.constraintlayout.motion.widget.MotionLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        onStartScreen()
+    }
+
+    private fun onStartScreen(){
+        val motionStart = findViewById<MotionLayout>(R.id.motion_start)
+        //Start Screen
+        Handler().postDelayed(Runnable {
+            //Врубаем анимацию
+            motionStart.transitionToEnd()
+            //Таймер окончания анимации
+            Handler().postDelayed(Runnable {
+                //Делаем screen не активным
+                motionStart.visibility = View.GONE
+            }, 600)
+        }, 1500)
     }
 }
